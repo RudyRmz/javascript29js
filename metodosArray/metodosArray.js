cdd5e63d4c23bbcc6d2aff41ce126148d543fe48
Rodolfo Ramirez <rudy.ramirez.morales@gmail.com>
2023-10-24 17:01:29 -0600

    se añadieron modificaciones a metodosArray.js

--- metodosArray/metodosArray.js
+++ metodosArray/metodosArray.js
@@ -17,22 +17,20 @@ let students = [
     {name:"Jorge", lastName: "Juarez Reyes"},
     {name:"Ulises", lastName: "Flores Grande"},
     {name:"Anais", lastName: "Laguna Torres"},
+    {name:"Ricardo", lastName: "Gomez Duran"},
+    {name:"Jose", lastName: "Sanchez Cruz"},
+    {name:"Eduardo", lastName: "Montiel Luna"},
 ];
 
 const initialsLetter = (studentsArray) =>{
     let initials = []
-    studentsArray.forEach((student, index, array)=>{
-        initials.push(`${student.name.charAt(0) + "."} ${student.lastName.split(" ")[0].slice(0,1)+"."} ${student.lastName.split(" ")[1].slice(0,1)+"."}`)
+    studentsArray.forEach((student)=>{
+        initials.push(`${student.name.charAt(0) + "."} ${student.lastName.charAt(0) + "."} ${student.lastName.split(" ")[1].slice(0,1) + "."}`)
     }) 
     console.log(initials)
-    
 }
 
-initialsLetter(students)
+let initials = initialsLetter(students);
 
-let arrayNombres = nombreCompleto.split(" ")
-    let iniciales = "";
-    for (let i = 0; i < arrayNombres.length ; i++){
-        iniciales += arrayNombres.charAt(0) + ". "
-    }
-    return iniciales;
+
+
